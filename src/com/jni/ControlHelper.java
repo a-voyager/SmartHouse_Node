a7bef8ad2f235496be98423682cd0799be12a497
wuhaojie <w19961009@126.com>
2016-07-05 13:52:28 +0800

    add jni.

---
+++ src/com/jni/ControlHelper.java
@@ -0,0 +1,11 @@
+package com.jni;
+
+/**
+ * Author: wuhaojie
+ * E-mail: w19961009@126.com
+ * Date: 2016/7/5 12:14
+ * Version: 1.0
+ */
+public class ControlHelper {
+    public static native void openLED();
+}
